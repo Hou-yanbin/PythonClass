5c3917d0e58455f20f9e98552f74796c221b79dd
Jack hou <256595662@qq.com>
2021-03-15 14:31:57 +0800

    课上练习碎代码

--- Test/函数f(x).py
+++ Test/函数f(x).py
@@ -4,10 +4,10 @@
 #@File: 函数f(x).py
 #@Software: PyCharm
 
-import math
-x=float(input())
-f=(math.sin(35)+((math.pow(math.e,x)-15*x)/(math.sqrt(math.pow(x,4)+1)))-math.log(7*x))
-print(round(f,3))
+# import math
+# x=float(input())
+# f=(math.sin(35)+((math.pow(math.e,x)-15*x)/(math.sqrt(math.pow(x,4)+1)))-math.log(7*x))
+# print(round(f,3))
 
 # a=int(input())
 # hour=int(a/60/60)%24
@@ -28,3 +28,40 @@ print(round(f,3))
 # # 计算面积
 # area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
 # print('%0.1f' % area)
+# f=int(input())
+# c=5/9*(f-32)
+# print(round(c,1))
+
+# def max_and_min(list):
+#  max_num=max(list1)
+#  min_num=min(list1)
+#  return ("Max=%d\nMin=%d"%(max_num,min_num))
+# ipt =input()
+# list1 =list(map(int,ipt.split(" ")))
+# print(max_and_min(list1))
+
+
+# ipt =input()
+# list1 =list(map(int,ipt.split(",")))
+
+# list1=[1,5,3,8]
+# # print(sorted(list1))
+# list2=sorted(list1)
+# n=len(list2)
+# for i in n:
+#     print(list2)
+# print(0b10101)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
